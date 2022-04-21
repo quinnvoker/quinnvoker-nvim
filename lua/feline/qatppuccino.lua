164d9c51ffdd5e00a646af2242d09c1aa7ba6234
Quinn <quinnvoker@gmail.com>
2022-04-20 22:11:02 -0700

    More fun with custom icon

--- lua/feline/qatppuccino.lua
+++ lua/feline/qatppuccino.lua
@@ -1,6 +1,20 @@
+local vim = require('vim')
 local components = require('catppuccin.core.integrations.feline')
 
 --- Replace main icon
-components.active[1][2].provider = ""
+components.active[1][2].provider = function()
+  local mode = vim.api.nvim_get_mode().mode
+  if mode == "i" then
+    return ""
+  elseif mode == "v" then
+    return ""
+  elseif mode == "c" then
+    return ""
+  elseif mode == "n" then
+    return ""
+  else
+    return ""
+  end
+end
 
 return components
