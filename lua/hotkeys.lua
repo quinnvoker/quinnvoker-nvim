6e17b54002f2bb83daf80018453f35fd14823b4d
Quinn <quinnvoker@gmail.com>
2022-04-21 18:55:52 +0000

    Add hotkey for vertical window creation

--- lua/hotkeys.lua
+++ lua/hotkeys.lua
@@ -1,4 +1,4 @@
--- Hotkey script
+-- Hotkey sCRipt
 local qkeymap = function(mode, key, result, opts)
   opts = opts or {noremap = true, silent = true}
   vim.api.nvim_set_keymap(
@@ -15,17 +15,20 @@ local expr_opts = {
   expr = true
 }
 
+-- Window
+qkeymap('', '<C-w>v', '<Esc>:vnew<CR>')
+
 -- NvimTree
-qkeymap('', '<leader>n', ':NvimTreeFocus<cr>')
-qkeymap('', '<C-n>', ':NvimTreeToggle<cr>')
-qkeymap('', '<C-f>', ':NvimTreeFindFile<cr>')
-qkeymap('', '<leader>r', ':NvimTreeRefresh<cr>')
+qkeymap('', '<Leader>n', ':NvimTreeFocus<CR>')
+qkeymap('', '<C-n>', ':NvimTreeToggle<CR>')
+qkeymap('', '<C-f>', ':NvimTreeFindFile<CR>')
+qkeymap('', '<Leader>r', ':NvimTreeRefresh<CR>')
 
 -- Telescope
-qkeymap('', '<leader>ff', ':Telescope find_files<cr>')
-qkeymap('', '<leader>fg', ':Telescope live_grep<cr>')
-qkeymap('', '<leader>fb', ':Telescope buffers<cr>')
-qkeymap('', '<leader>fh', ':Telescope help_tags<cr>')
+qkeymap('', '<Leader>ff', ':Telescope find_files<CR>')
+qkeymap('', '<Leader>fg', ':Telescope live_grep<CR>')
+qkeymap('', '<Leader>fb', ':Telescope buffers<CR>')
+qkeymap('', '<Leader>fh', ':Telescope help_tags<CR>')
 
 -- cmp + autopairs
 qkeymap('i', '<CR>', 'v:lua.MUtils.completion_confirm()', expr_opts)
@@ -39,12 +42,12 @@ qkeymap('n', 'gW', ':lua vim.lsp.buf.workspace_symbol()<CR>')
 qkeymap('n', 'gr', ':lua vim.lsp.buf.references()<CR>')
 qkeymap('n', 'gt', ':lua vim.lsp.buf.type_definition()<CR>')
 qkeymap('n', 'K', ':lua vim.lsp.buf.hover()<CR>')
-qkeymap('n', '<c-k>', ':lua vim.lsp.buf.signature_help()<CR>')
-qkeymap('n', '<leader>af', ':lua vim.lsp.buf.code_action()<CR>')
-qkeymap('n', '<leader>rn', ':lua vim.lsp.buf.rename()<CR>')
+qkeymap('n', '<C-k>', ':lua vim.lsp.buf.signature_help()<CR>')
+qkeymap('n', '<Leader>af', ':lua vim.lsp.buf.code_action()<CR>')
+qkeymap('n', '<Leader>rn', ':lua vim.lsp.buf.rename()<CR>')
 
 --- Formatter
-qkeymap('n', '<leader>FF', ':Format<CR>')
+qkeymap('n', '<Leader>FF', ':Format<CR>')
 
 --- Terminal
 qkeymap('t', '<Esc>', '<C-\\><C-n>')
