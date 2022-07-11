045a2b8905ebe59053d00e4cb8d16a24f6dc0a3d
Quinn <quinnvoker@proton.me>
2022-07-10 19:38:27 -0700

    Disable friendly-snippets for now

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -67,7 +67,7 @@ packer.startup(function(use)
 				"L3MON4D3/LuaSnip",
 				after = "nvim-cmp",
 				config = function()
-					require("luasnip.loaders.from_vscode").lazy_load()
+					-- require("luasnip.loaders.from_vscode").lazy_load()
 					require("luasnip").filetype_extend("ruby", { "rails" })
 					require("snippets")
 				end,
